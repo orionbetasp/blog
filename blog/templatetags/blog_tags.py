18ff0857b15cdd79e7779f3d5fddb656f11d0e02
unknown <2899592919@qq.com>
2019-02-27 16:45:06 +0800

    修改标签云

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from django.db.models.aggregates import Count
-from ..models import Post, Category
+from ..models import Post, Category, Tag
 
 register = template.Library()
 
@@ -22,3 +22,7 @@ def get_categories():
 @register.simple_tag
 def get_categories():
     return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
