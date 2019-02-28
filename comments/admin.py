8941a32528a7e4d9731acde63902782bf97a7b64
unknown <2899592919@qq.com>
2019-02-28 17:05:10 +0800

    修改评论项

--- comments/admin.py
+++ comments/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Comment
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'url', 'text', 'created_time']
+    list_display = ['name', 'email', 'text', 'created_time']
 
 admin.site.register(Comment, CommentAdmin)
